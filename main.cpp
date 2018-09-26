7439cb0fe41e7ae36428d7d29279823768655ff3
ajmoebius <ajmoebius@ecc-jaguar.csuchico.edu>
2018-09-26 16:34:08 -0700

    completed

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//Author: Aaron Moebius
+
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,7 +9,11 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  string newItem;
+  int index;
+  
+  do
+  {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
@@ -15,5 +21,29 @@ int main()
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
 
+  if(input=='A')
+  {
+    if(numItems<4)
+    {
+      cout<<"What is the item?"<<endl;
+      cin>>newItem;
+      list[numItems]=newItem;
+      numItems++;
+      
+      index=0;
+      while(index<=numItems)
+      {
+      cout<<list[index];
+      index++;
+      }
+    }
+    else
+    {
+      cout<<"You'll need a bigger list!"<<endl;
+      cout<<list;
+    }
+  }
+
+  }while(input!='Q');
   return 0;
 }
